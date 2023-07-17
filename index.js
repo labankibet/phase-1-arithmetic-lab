22f54b0bd4c6a107eeefac65e7731cecf97721b5
labankibet <laban.kibet@moringaschool.com>
2023-07-17 08:04:33 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+console.log(multiply);
+
+
+
+let random = Math.floor(Math.random()*
+Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random)
+
+
+let num3 = 14;
+let num4 = 7;
+let mod = (num3 % num4) + 4;
+console.log(mod);
+
+
+let numbers = [10,5,20,15,8];
+let max = Math.max(...numbers);
+
+console.log(max);
+
+
